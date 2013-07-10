31da9f037347a97e601a2ce87e6ed255922160d2
Nathan Hollerbach <nathanhollerbach@gmail.com>
2013-07-09 22:21:00 -0500

    Associated users and posts...cool stuff.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  has_many :posts
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
